f5e3db53e0a01569936c007684a62b8084908c89
kywk23 <kelvinywk23@gmail.com>
2023-10-17 12:20:31 +0800

    beat-that project, player 1 part completed

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
+// BEAT THAT!
+// There are 2 players and players take turns.
+// When a player clicks Submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
+// The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
+// After both players have rolled and chosen dice order, the player with the higher combined number wins.
+
+// Dice randomizing helper
+var randomDiceRolls = function () {
+  var randomInterger = Math.floor(Math.random() * 6) + 1;
+  return randomInterger;
+};
+
+// Player Roll Dice
+var playerRollDice = function () {
+  var counter = 0;
+  while (counter < 2) {
+    playerDiceArray.push(randomDiceRolls());
+    counter += 1;
+  }
+  return playerDiceArray[0], playerDiceArray[1];
+};
+
+// Global Variables
+var gameState = "diceRoll";
+var myOutputValue = "";
+var playerDiceArray = [];
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (gameState == "diceRoll") {
+    playerRollDice();
+    console.log(`game state is ${gameState}`);
+    console.log(playerDiceArray);
+    gameState = "chooseDiceSequence";
+    // GAME STATE CHANGED TO DICE SEQUENCE
+    return (
+      `Player 1 rolled ${playerDiceArray[0]} and ${playerDiceArray[1]}` +
+      `<br><br> Please choose the sequence of your number by inputing 1 or 2`
+    );
+  } else if (gameState === "chooseDiceSequence") {
+    if (input == 1) {
+      var playerScore = Number(
+        String(playerDiceArray[0]) + String(playerDiceArray[1])
+      );
+      console.log(playerScore); // Log playerScore
+      return `Your Score is ${playerScore}`;
+    } else if (input == 2) {
+      var playerScore = Number(
+        String(playerDiceArray[1]) + String(playerDiceArray[0])
+      );
+      console.log(playerScore); // Log playerScore
+      return `Your Score is ${playerScore}`;
+    }
+  }
 };
