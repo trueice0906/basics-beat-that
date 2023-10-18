34cf1e95b5c42283149b3a8cd6d1b1db5fd6f689
Yen <yenyen_choo@singaporeair.com.sg>
2023-10-18 21:34:07 +0800

    tmp

--- script.js
+++ script.js
@@ -6,101 +6,58 @@
 
 // Dice randomizing helper
 var randomDiceRolls = function () {
-  return Math.floor(Math.random() * 6) + 1;;
+  var randomInterger = Math.floor(Math.random() * 6) + 1;
+  return randomInterger;
 };
 
 // Player Roll Dice
 var playerRollDice = function () {
-  var rollTimes = 2;
-  playerDiceArray = [];
-  for (let i = 0; i < rollTimes; i++) {
+  var counter = 0;
+  while (counter < 2) {
     playerDiceArray.push(randomDiceRolls());
+    counter += 1;
   }
-  return playerDiceArray;
+  return [playerDiceArray[0], playerDiceArray[1]];
 };
-
-var checkScore = function() {
-  var winning;
-  if (Number(scoreBoard[0]) > Number(scoreBoard[1])) {
-    winning = 'Player 1 won!'
-  } else {
-    winning = 'Player 2 won!'
-  }
-  return `Player 1: ${scoreBoard[0]}, Player 2: ${scoreBoard[1]}, ${winning}`;
-}
-
-var arrangeSequence = function(array, input) { // [1,2]
-  playerScore = `${array[Number(input[0])-1]}${array[Number(input[1])-1]}`
-  scoreBoard[activePlayer] = playerScore;
-}
-
-var switchPlayer = function() {
-  activePlayer = activePlayer === 0? 1 : 0;
-  dice1.innerHTML = '';
-  dice2.innerHTML = '';
-}
-
-var init = function() {
-  activePlayer = 0;
-  scoreBoard = [0,0];
-  playerDiceArray = [];
-  playerScore = 0;
-  diceArray = [];
-  title.innerHTML = `Player ${activePlayer+1}`;
-  dice1.innerHTML = '';
-  dice2.innerHTML = '';
-  output.innerHTML = '';
-}
 
 // Global Variables
-var playerDiceArray;
-var playerScore;
-var activePlayer = 0;
-var scoreBoard = [0,0]
-var diceArray;
-var output = document.querySelector("#output-div");
-var rollBtn = document.querySelector("#roll-btn");
-var dice1 = document.querySelector("#dice1");
-var dice2 = document.querySelector("#dice2");
-var input = document.querySelector("#input-field");
-var button = document.querySelector("#submit-button");
-var title = document.querySelector("#player-title");
-var restart = document.querySelector("#restart-btn");
+var gameState = "diceRollState";
+var myOutputValue = "";
+var playerDiceArray = [];
+var currentPlayer = 1;
+var scoreBoard = [0,0];
 
-var main = function () {
-
-  rollBtn.addEventListener("click", function() {
-    // get roll dice result , return in array , for eg [4,5];
-    diceArray = playerRollDice();
-    dice1.innerHTML = `Dice 1: ${diceArray[0]}`;
-    dice2.innerHTML = `Dice 2: ${diceArray[1]}`
-  })
-
-  button.addEventListener("click", function () {
-    dice1.innerHTML = '';
-    dice2.innerHTML = '';
-
-    // get user sequence input  
-    // input form = first dice, second dice, for eg,2,1
-    // split the input value to return sequence in array , for eg, ['2','1']
-    // arrange the sequence to get the biggest number
-
-    var sequence = input.value.split(',');
-    var result = arrangeSequence(diceArray, sequence);
-    input.value = ""; // clear input field
-
-
-    if (activePlayer == 1) {
-      result = checkScore();
-      output.innerHTML = result;
-    } else {
-      switchPlayer();
-      title.innerHTML = `Player ${activePlayer + 1}`
-    }
-  });
+var playerChooseDiceSequence = function (input) {
+  if (input == 1) {
+    var playerScore = Number(
+      String(playerDiceArray[0]) + String(playerDiceArray[1])
+    )
+  } else {
+    var playerScore = Number(
+      String(playerDiceArray[1]) + String(playerDiceArray[0])
+    );
+  }
+  scoreBoard[currentPlayer] = playerScore;
+  return `Your Score is ${playerScore}`;
 };
 
-restart.addEventListener('click', function() {
-  init();
-})
+var main = function (input) {
 
+    if (gameState == "diceRollState") {
+      playerRollDice();
+      console.log(`game state is ${gameState}`);
+      gameState = "chooseDiceSequenceState"; // GAME STATE CHANGED TO CHOOSE-DICE SEQUENCE
+      console.log(`game state is ${gameState}`);
+      myOutputValue =
+        `Player 1 rolled ${playerDiceArray[0]} and ${playerDiceArray[1]}` +
+        `<br><br> Player ${currentPlayer}, please choose the sequence of your number by inputting 1 or 2`;
+    } else if (gameState === "chooseDiceSequenceState") {
+      myOutputValue = playerChooseDiceSequence(input);
+      currentPlayer = currentPlayer === 1 ? 2 : 1;
+      gameState = "diceRollState";
+      playerDiceArray = [];
+      console.log(`current player is ${currentPlayer}`);
+      console.log(gameState);
+    }
+    return myOutputValue;
+};
